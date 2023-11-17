ebe962a6640cf9523de508adebe1cb0d11dfae15
Neelakandan C <neelakandan9854@gmail.com>
2023-11-17 05:09:32 +0000

    changes

---
+++ tests/assert_stg_payments_amount_is_possitive.sql
@@ -0,0 +1,13 @@
+with payments as(
+    select * from {{ ref('stg_payments') }}
+)
+
+select 
+    order_id,
+    sum(amount) as total_amount
+
+from payments
+
+group by 1
+having total_amount < 0
+
